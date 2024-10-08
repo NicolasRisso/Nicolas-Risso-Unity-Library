91d95cbdc8ce7008910176660072e39fc8bf9180
Nicolas Risso <105318465+NicolasRisso@users.noreply.github.com>
2024-10-08 14:20:06 -0300

    Added singletons folder

---
+++ Singletons/SingletonPersistent.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+
+/// <summary> Persistant and Generic Singleton Class </summary>
+/// <typeparam name="T">Component type to make persistent singleton instance</typeparam>
+public class SingletonPersistent<T> : MonoBehaviour where T : Component
+{
+    public static T _instance;
+
+    /// <summary> Retrives Singleton instance. If there is none, creates a new GO and class component. </summary>
+    /// <value> Returns class instance </value>
+    public static T Instance
+    {
+        get
+        {
+            if (_instance == null)
+            {
+                GameObject go = new GameObject();
+                go.name = typeof(T).Name;
+                go.hideFlags = HideFlags.HideAndDontSave;
+                _instance = go.AddComponent<T>();
+                DontDestroyOnLoad(go);
+            }
+            return _instance;
+        }
+        private set { }
+    }
+}
+
